7e6061e07d6d304d04a3ede0374f6fb9cb5b83b0
Tejas Bhosale <bhosaletejas93@gmail.com>
2019-01-10 22:13:12 +0530

    basic server and app setup

--- server/index.js
+++ server/index.js
@@ -1,1 +1,22 @@
-// This is where we start.
+// This is where we start with our server.
+const express = require("express");
+const http = require("http");
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
+
+const app = express();
+
+//App setup.
+app.use(morgan("combined"));
+app.use(
+  bodyParser.json({
+    type: "*/*"
+  })
+);
+
+//server setup.
+
+const port = process.env.PORT || 3000;
+const server = http.createServer(app);
+server.listen(port);
+console.log("Server listening on: ", port);
